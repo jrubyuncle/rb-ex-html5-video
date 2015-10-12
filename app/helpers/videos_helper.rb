bc2220346fd09dbb609feac9f1982d287b287876
Wesley Ju <jrubyuncle@gmail.com>
2015-10-12 18:11:10 +0800

    instead of hard-coding, call print_all_tags in view

--- app/helpers/videos_helper.rb
+++ app/helpers/videos_helper.rb
@@ -1,2 +1,10 @@
 module VideosHelper
+  def print_all_tags
+    total = ''
+    4.times do
+      total += "\n<br>"
+      total += video_tag("/en_rails_stukio_ajax.mp4", width: '320', height: '240', controls: true)
+    end
+    total
+  end
 end
