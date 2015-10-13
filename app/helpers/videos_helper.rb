05056408c127ef65de022c24c68a560ed3aa7c50
guest2 <guest2@u14.04-desk-64-1>
2015-10-13 12:30:03 +0800

    call print_all_tags in view

--- app/helpers/videos_helper.rb
+++ app/helpers/videos_helper.rb
@@ -1,38 +1,14 @@
 module VideosHelper
   def print_all_tags
-    #vhash = '2Il7PPhen3o'
-    #title = 'stuk.io, rails ajax'
     duration_hash = {
-      '0m0s' => 'start',
-      '0m7s' => 'go to railsguides, working with javascript in rails',
-      '0m19s' => 'waht is ajax, javascript has two powerful fearutes',
-      '1m05s' => 'this is the app going to build',
-      '2m18s' => 'so lets begin, rails new my_ajax',
-      '2m58s' => 'rails g scaffold Post title:string content:text',
-      '3m25s' => 'rake db:migrate, since scaffold create migration, rails s',
-      '3m48s' => 'opne sublime text, in routes.rb, root posts#index',
-      '4m35s' => 'do some styling',
-      '5m07s' => 'begin on the sublime, go to getbootstrap/get started, copy bootstrap cdn to application.html.erb',
-      '5m45s' => 'lets do some changes to our views, copy to index.html.erb from http://getbootstrap.com/javascript/#modals-examples',
-      '6m23s' => 'delete content of index.html.erb, create new partial _post.heml.erb, data-target=#mynew_post, id=container_posts, id=mynew_post, ',
-      '8m08s' => '_post.html.erb, div id=post_<%= post.id %>',
-      '10m10s' => 'bug, @post = Post.new',
-      '11m12s' => '*** has finished styling the app, back to rails guides, remote: true, format.js, create.js.erb',
-      '12m30s' => '*** 3 steps, 1. in form_for, add remote: true, 2. in posts controller, add format.js',
-      '13m07s' => '*** the 3rd step, create create.js.erb, alert("post created")',
-      '13m43s' => 'test',
-      '14m37s' => 'the 1st thing to close the modal, ',
-      '15m16s' => 'empty post_title post_content',
-      '16m04s' => '$("#container_posts").prepend()',
-      '18m56s' => 'check it',
-      '19m29s' => '*** 3 steps',
-      '19m58s' => 'do the same for update and destroy',
-      '23m18s' => 'test the update',
-      '23m50s' => 'for the destroy',
-      '25m03s' => 'test the destroy',
-      '25m32s' => 'ok, the end'
+        '0m00s' => 'start',
+        '0m11s' => 'create branch',
+        '0m38s' => 'rebase',
+        '1m29s' => 'git checkout, git rebase',
+        '2m48s' => 'most common in open source project',
+        '3m16s' => 'merges',
+        '3m40s' => 'rebase v.s. merge'
     }
-
     total = ''
     duration_hash.keys.each_with_index do |key, index|
       order = index.to_i + 1
@@ -43,7 +19,7 @@ module VideosHelper
 
       total += "\n<hr>#{order}/#{duration_hash.size}: #{key}"
       total += "<br>"
-      total += video_tag("/en_rails_stukio_ajax.mp4\#t=#{start_time_in_sec}", width: '320', height: '240', controls: true)
+      total += video_tag("/git_rebase.mp4\#t=#{start_time_in_sec}", width: '320', height: '240', controls: true)
     end
     total
   end
